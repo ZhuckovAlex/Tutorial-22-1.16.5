cc32a92d6d68e49eb79aaa2ac6bfc6d4e2c38558
ek4teeliseeva <77374953+ek4teeliseeva@users.noreply.github.com>
2021-12-28 00:36:50 +0300

    Tutorial-22-1.16.5

---
+++ src/main/java/net/project/project/tileentity/ModTileEntities.java
@@ -0,0 +1,23 @@
+package net.project.project.tileentity;
+
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.project.project.Project;
+import net.project.project.block.ModBlocks;
+
+public class ModTileEntities {
+
+    public static DeferredRegister<TileEntityType<?>> TILE_ENTITIES =
+            DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, Project.MOD_ID);
+
+    public static RegistryObject<TileEntityType<VerstakTile>> VERSTAK_TILE =
+            TILE_ENTITIES.register("verstak_tile", () -> TileEntityType.Builder.create(
+                    VerstakTile::new, ModBlocks.VERSTAK.get()).build(null));
+
+    public static void register(IEventBus eventBus) {
+        TILE_ENTITIES.register(eventBus);
+    }
+}
